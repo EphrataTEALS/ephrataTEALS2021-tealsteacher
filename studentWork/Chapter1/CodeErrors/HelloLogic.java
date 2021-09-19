8a512bcde177c6594c08628af6ba986331239281
tealsteacher <darsch@ephrataschools.org>
2021-09-19 20:22:07 +0000

    Logic

---
+++ studentWork/Chapter1/CodeErrors/HelloLogic.java
@@ -0,0 +1,9 @@
+package studentWork.Chapter1.CodeErrors;
+
+public class HelloLogic {
+    public static void main(String[] args) {
+        System.out.println("Hello, "
+        	                + "world!");
+    }
+}
+    
