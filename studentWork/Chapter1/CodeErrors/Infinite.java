5d80234ad2f9e0d1ab5c8821cb09f0a0d7c73430
tealsteacher <darsch@ephrataschools.org>
2021-09-19 20:17:12 +0000

    infinite

---
+++ studentWork/Chapter1/CodeErrors/Infinite.java
@@ -0,0 +1,13 @@
+package studentWork.Chapter1.CodeErrors;
+
+public class Infinite {
+    public static void main(String[] args) {
+    oops();
+    }
+        
+    public static void oops(){
+        System.out.println("Make it Stop!");
+        opps();
+    }
+}
+    
