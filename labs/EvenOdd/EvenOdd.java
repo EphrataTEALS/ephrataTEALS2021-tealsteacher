aeab68841c67424fa066c173d8dd82ca8864223b
tealsteacher <darsch@ephrataschools.org>
2021-10-15 15:58:25 +0000

    scaf

---
+++ labs/EvenOdd/EvenOdd.java
@@ -0,0 +1,21 @@
+/**
+ * Prints out if number is even or odd to the console.
+ * 
+ * @author Darwin
+ */
+
+package labs.EvenOdd;
+
+public class EvenOdd {
+    
+    public static void main(String[] args){
+
+        // Create a variable for an integer number
+
+        
+
+        // Create If Else Logic
+
+
+    }
+}
