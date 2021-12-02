92b95c24d3895807070d4214b4e704592f6f4f33
tealsteacher <darsch@ephrataschools.org>
2021-12-02 23:53:43 +0000

    working

--- sampleCode/CumulativeAlgo/HockeyStats.java
+++ sampleCode/CumulativeAlgo/HockeyStats.java
@@ -7,7 +7,7 @@ public class HockeyStats {
     public static void main(String[] args) {
         
         Scanner team = new Scanner(System.in);
-        Scanner hstat = new Scanner(System.in);
+        Scanner periods = new Scanner(System.in);
         
         System.out.println("What Teams are playing? ");
         System.out.println("Home Team? ");
@@ -16,24 +16,32 @@ public class HockeyStats {
         String team2 = team.next();
 
         System.out.println("How many periods have been played so far? ");
-        int periodsPlayed = hstat.nextInt();
+        int periodsPlayed = periods.nextInt();
         //System.out.println("Number of periods played: " + periodsPlayed);
 
-        int sum = 0;
-        double average = 0;
+        //String homeTeam = TotalGoals(team1, periodsPlayed);
+        //String awayTeam = TotalGoals(team2, periodsPlayed);
+
+        System.out.println(TotalGoals(team1, periodsPlayed));
+        System.out.println();
+        System.out.println(TotalGoals(team2, periodsPlayed));
+
+
     }
 
-        public static int TotalGoals(String teamName, int periods) {
+        public static String TotalGoals(String teamName, int periods) {
             
-        for (int i=1; i <= periods; i++) {
-            System.out.println("What was the number of goals at the end of the " + i + " period? ");
-            int value = hstat.nextInt();
-            sum = sum + value;
-            average = sum/(double)periodsPlayed;
-        }
+            int sum = 0;
+            double average = 0.0;
+            for (int i=1; i <= periods; i++) {
+                Scanner hstat = new Scanner(System.in);
+                System.out.println("The " + teamName + " scored how many goals at the end of the " + i + " period? ");
+                int value = hstat.nextInt();
+                sum = sum + value;
+                average = sum/(double)periods;
+            }
 
-        System.out.println("The total number of goals is " + sum);
-        System.out.println("The average number of goals is " + average);
+            return "The " + teamName + " have " + sum + " goals and an average of " + average + " goals per period";
         
     } 
 }
